11666a7c64d92ab89891c497cd7bb07d670daf88
SM-Fahad <fahad.dev2024@gmail.com>
2025-12-14 11:13:37 +0600

    cros add

--- src/main/java/com/ashtana/backend/secqurity/SecurityConfig.java
+++ src/main/java/com/ashtana/backend/secqurity/SecurityConfig.java
@@ -87,7 +87,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedOriginPatterns(List.of("*","https://ashtana-render.onrender.com/**"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
